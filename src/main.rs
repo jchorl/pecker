f09afefeec66255ddba99f87b22c8482c5c52f5f
Josh Chorlton <jchorlton@gmail.com>
2020-04-22 20:11:04 -0700

    seems to build

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate prometheus;
+
 use clap::Clap;
+use prometheus::Gauge;
 use std::io;
 use std::process::Command;
+use std::time::{SystemTime, UNIX_EPOCH};
 use std::{thread, time};
 
 #[derive(Clap)]
@@ -11,19 +18,31 @@ struct Opts {
     drives: Vec<String>,
 }
 
+lazy_static! {
+    static ref LAST_COMPLETED_GAUGE: Gauge = register_gauge!(
+        "pecker_last_completed_epoch_seconds",
+        "The time of last completion"
+    )
+    .unwrap();
+}
+
 fn main() {
     let opts: Opts = Opts::parse();
 
-    std::process::exit(match run_app(opts.drives, opts.metrics_addr) {
-        Ok(_) => 0,
+    std::process::exit(match run_app(opts.drives) {
+        Ok(_) => {
+            let _ = meter_completion(&opts.metrics_addr, true);
+            0
+        }
         Err(err) => {
+            let _ = meter_completion(&opts.metrics_addr, false);
             eprintln!("error: {:?}", err);
             1
         }
     });
 }
 
-fn run_app(drives: Vec<String>, metrics_addr: String) -> io::Result<()> {
+fn run_app(drives: Vec<String>) -> io::Result<()> {
     for d in &drives {
         let _ = run_cmd("smartctl", vec!["--test=short", &d])?;
     }
@@ -33,8 +52,6 @@ fn run_app(drives: Vec<String>, metrics_addr: String) -> io::Result<()> {
     for d in &drives {
         let _ = run_cmd("smartctl", vec!["-q", "errorsonly", "-a", &d])?;
     }
-
-    println!("would emit metrics to {}", metrics_addr);
 
     Ok(())
 }
@@ -47,4 +64,21 @@ fn run_cmd(cmd: &str, args: Vec<&str>) -> io::Result<()> {
     assert!(output.status.success());
 
     Ok(())
+}
+
+fn meter_completion(address: &str, success: bool) -> Result<(), prometheus::Error> {
+    LAST_COMPLETED_GAUGE.set(
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as f64,
+    );
+    let success_str = if success { "1" } else { "0" };
+    prometheus::push_metrics(
+        "pecker",
+        labels! {"success".to_owned() => success_str.to_owned(),},
+        address,
+        prometheus::gather(),
+        None,
+    )
 }
